9b0fe1a0099d0cf3b3fa2b0c098b72fe9c50af03
lisadebona <annalisa.quiamco@gmail.com>
2019-02-08 21:11:39 +0800

    Updated 404 page

--- 404.php
+++ 404.php
@@ -11,18 +11,10 @@ get_header(); ?>
 
 	<div id="primary" class="full-content-area default-temp clear">
 		<main id="main" class="site-main wrapper" role="main">
-
-			<p>We couldn't find the page you were looking for. This is either because:</p>
-			<ul>
-			  <li>There is an error in the URL entered into your web browser. Please check the URL and try again.</li>
-			  <li>The page you are looking for has been moved or deleted.</li>
-			</ul>
-
-			<p>
-			  You can return to our homepage by <a href="<?php echo get_site_url();?>">clicking here</a>, or you can try searching for the
-			  content you are seeking by <a href="<?php echo get_site_url();?>/search">clicking here</a>.
-			</p>
-
+			<?php
+				$page_content = get_field('page_404_content','option');
+				echo $page_content;
+			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
