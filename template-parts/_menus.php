5848fe4afd15704ffa5f696bfa291320ac0d775c
Lisa <annalisa.quiamco@gmail.com>
2019-12-17 21:27:19 -0700

    Added new MENU (4-column layout)

--- template-parts/_menus.php
+++ template-parts/_menus.php
@@ -5,6 +5,7 @@ $post_id = $post->ID;
 $content = apply_filters( 'the_content', $post->post_content ); ?>
 
 <section id="menus" class="section text-center menus clear">
+
 	<div class="wrapper">
 		<div class="maintext clear"><?php echo $content; ?></div>
 		<?php
@@ -17,21 +18,20 @@ $content = apply_filters( 'the_content', $post->post_content ); ?>
 			<img src="<?php echo $content_image['url'];?>" alt="<?php echo $content_image['title'];?>" />
 		</div>
 		<?php } ?>	
-
-		<?php if($menus) { ?>
-		<div class="food-menu clear">
-			<ul>
-			<?php foreach($menus as $m) {
-				$menu_name = $m['name'];
-				$menu_link = ($m['link']) ? $m['link']['url'] : '#'; 
-				?>
-				<li><a class="btn-red-border" href="<?php echo $menu_link; ?>" target="_blank"><?php echo $menu_name; ?></a></li>
-			<?php } ?>	
-			</ul>
-		</div>
-		<?php } ?>	
-
-		
 	</div>
+	
+	<?php if($menus) { ?>
+	<div class="food-menu clear">
+		<ul class="menuInfo">
+		<?php foreach($menus as $m) {
+			$menu_name = $m['name'];
+			$menu_link = ($m['link']) ? $m['link']['url'] : '#'; 
+			?>
+			<li><a class="btn-red-border" href="<?php echo $menu_link; ?>" target="_blank"><?php echo $menu_name; ?></a></li>
+		<?php } ?>	
+		</ul>
+	</div>
+	<?php } ?>	
+	
 </section>
 <?php } ?>
