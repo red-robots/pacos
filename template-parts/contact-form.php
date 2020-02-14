58153a82c5b0ccd89b7d43ba3795974527dc9e91
Lisa <lisaqdebona@gmail.com>
2020-02-13 22:22:49 -0700

    Fixed sticky contact form

--- template-parts/contact-form.php
+++ template-parts/contact-form.php
@@ -1,15 +1,16 @@
 <?php
 	$contact_text = get_field('contact_text','option');
-	$contact_form_shortcode = get_field('contact_form_shortcode','option');
+	//$contact_form_shortcode = get_field('contact_form_shortcode','option');
+	$contactformID = get_field('contact_form_shortcode','option');
 ?>
-<?php if($contact_form_shortcode && do_shortcode($contact_form_shortcode) ) { ?>
+<?php if($contactformID && do_shortcode('[gravityform id="'.$contactformID.'" title="false" description="false" ajax="true"]') ) { ?>
 <div class="sticky-contact-form">
 	<div class="wrapp clear">
 		<div id="formTitleDiv" class="titlediv clear"><a id="contactFormPp" href="#"><span class="txt">Contact Us</span><span class="icon"><i class="fal fa-angle-up"></i></span></a></div>
 		<div id="formContents" class="inside-wrap clear">
 			<div class="inside clear">
 				<?php if($contact_text) { ?><div class="note"><?php echo $contact_text; ?></div><?php } ?>
-				<?php echo do_shortcode($contact_form_shortcode); ?>
+				<?php echo do_shortcode('[gravityform id="'.$contactformID.'" title="false" description="false" ajax="true"]'); ?>
 			</div>
 		</div>
 	</div>
